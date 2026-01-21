96f5250f2b965861418096e273cdca9f36ee7622
niloadarve <nilo.jan.adarve@gmail.com>
2026-01-21 06:16:53 +0000

    Deploying to gh-pages from @ niloadarve/keystonetutors@37365b212f22b7cf1636e8f62c5d17e7113fdc65 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/keystonetutors/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/keystonetutors/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
